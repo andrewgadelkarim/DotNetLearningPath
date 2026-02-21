97d95bf72f8a98708a01cb7e0d4668b306d55db6
Andrew-Amgad-Wahba <andrew.wahba@flairstech.com>
2026-02-21 15:58:38 +0200

    Add project files.

---
+++ EFCoreInActionBook/MyFirstEFCoreApp/MyFirstEFCoreApp/MyFirstEFCoreApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstEFCoreApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
