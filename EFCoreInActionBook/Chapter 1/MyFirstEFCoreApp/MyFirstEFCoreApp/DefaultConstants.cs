7761d28de3e50bcd64601b79eb4670368e69af85
Andrew-Amgad-Wahba <andrew.wahba@flairstech.com>
2026-02-25 17:04:34 +0200

    Chapter completion

---
+++ EFCoreInActionBook/Chapter 1/MyFirstEFCoreApp/MyFirstEFCoreApp/DefaultConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstEFCoreApp
+{
+    public static class DefaultConstants
+    {
+        public const string DefaultUrlPropertyConst = "- no web URL given -";
+    }
+}
---
+++ EFCoreInActionBook/Chapter 1/MyFirstEFCoreApp/MyFirstEFCoreApp/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace MyFirstEFCoreApp.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; } //Using this naming convention lets EF Core know automatically that this is the primary key
+        
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime PublishedOn { get; set; } //TODO: Look into using DateOnly to avoid timezone issues
+
+        public int AuthorId { get; set; } //Foreign Key to author table
+        public Author Author { get; set; } = new(); //Navigation property
+    }
+}
